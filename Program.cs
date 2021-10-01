cc946c89a58d51b36b0a30d5b502ec78b6b3aa1e
Atticus C <atticus@softwarefuze.com>
2021-10-01 07:42:05 -0700

    Add lookup command

--- Program.cs
+++ Program.cs
@@ -20,10 +20,16 @@ namespace DNS_Cacher {
             while (true) {
                 listener.listen(cache.currCache);
 
-                DNSEntry entry = DNSLookup.find(hostname: listener.currCommand);
+                if (listener.currCommand.StartsWith("lookup")) {
+                    DNSEntry entry = DNSLookup.find(hostname: listener.currCommand.Substring(listener.currCommand.IndexOf(" ") + 1));
 
-                if (entry != null) {
-                    entry.logInfo();
+                    if (entry != null) {
+                        entry.logInfo();
+                    }
+                } else {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine($"Invalid command. ({listener.currCommand})");
+                    Console.ForegroundColor = ConsoleColor.White;
                 }
             }
         }
